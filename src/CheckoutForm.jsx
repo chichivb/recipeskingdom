2be2d8e5eba911a42592e1aa9e1d43daaa6a7471
Chitralada pensuk <chitralada.pensuk@gmail.com>
2024-03-09 15:29:43 +0200

    Fix confirm url

--- src/CheckoutForm.jsx
+++ src/CheckoutForm.jsx
@@ -24,7 +24,7 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: `${window.location.origin}/#/completion`,
+        return_url: `${window.location.origin}/completion`,
       },
     });
 
