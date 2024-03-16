78bae6e772c9ce1b3f05e5015f9e8341f6b05a37
Chitralada pensuk <chitralada.pensuk@gmail.com>
2024-03-16 22:49:11 +0200

    Completion update

--- src/Completion.jsx
+++ src/Completion.jsx
@@ -1,6 +1,6 @@
 function Completion(props) {
   return (
-    <h1 className="text-4xl font-bold text-center text-green-600">
+    <h1 className="py-20 text-4xl font-bold text-center text-green-600">
       Thank you! 🎉
     </h1>
   );
