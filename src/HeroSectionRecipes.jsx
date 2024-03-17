bb68a282461ecd5fe5adab496176cfeb5cbec70b
Chitralada pensuk <chitralada.pensuk@gmail.com>
2024-03-17 10:35:44 +0200

    Update

--- src/HeroSectionRecipes.jsx
+++ src/HeroSectionRecipes.jsx
@@ -11,7 +11,7 @@ const HeroSectionRecipes = () => {
         <div className="grid grid-cols-2">
           <div className="col-span-1 flex flex-col justify-center">
             <h1 className="text-5xl font-bold text-gray-800 mb-4">
-              Eating Heathy is Simple
+              Eating Healthy is Simple
             </h1>
 
             <p className="text-xl text-gray-600 mb-8">
